69b385fa43374a6f87b112ffdb7c6f5b75bf172e
tatsunori_nishikori <tatsunori_nishikori@metaps.com>
2015-02-20 20:36:38 +0900

    add hadoop recipe

--- site-cookbooks/hadoop/attributes/default.rb
+++ site-cookbooks/hadoop/attributes/default.rb
@@ -5,7 +5,11 @@ default['java']['src'] = '/usr/local/src/'
 default['java']['home'] = '/usr/local/java/'
 
 # hadoop attributes
-default['hadoop']['package_name'] = 'cloudera-cdh-5-0.x86_64.rpm'
+default['hadoop']['package_name'] = 'hadoop-2.6.0.tar.gz'
+default['hadoop']['archive_name'] = 'hadoop-2.6.0.tar.gz'
 default['hadoop']['src'] = '/usr/local/src/'
 default['hadoop']['user'] = 'vagrant'
 default['hadoop']['group'] = 'vagrant'
+default['hadoop']['port'] = 9000 
+default['hadoop']['hdfs_dir_name'] = 'hadoopdata'
+default['hadoop']['home'] = "/home/#{default['hadoop']['user']}/hadoop-2.6.0"
--- site-cookbooks/hadoop/recipes/default.rb
+++ site-cookbooks/hadoop/recipes/default.rb
@@ -9,56 +9,74 @@
   end
 end
 
-# 2. hadoop rpm package setting
-#cookbook_file node['hadoop']['package_name'] do
-#  path "#{node['hadoop']['src']}#{node['hadoop']['package_name']}"
-##  source node['hadoop']['package_name']
-#  mode 0744
-#  action :create_if_missing
-#end
+# 2. hadoop archive setting
+cookbook_file node['hadoop']['archive_name'] do
+  path "#{node['hadoop']['src']}#{node['hadoop']['archive_name']}"
+#  source node['hadoop']['archive_name']
+  mode 0744
+  action :create_if_missing
+end
 
 
-# 2. hadoop add repos install
-#bash "hadoop_install" do
-#  user 'root'
-#  group 'root'
+# 3. hadoop install
+bash "hadoop_install" do
+  cwd "/home/#{node['hadoop']['user']}"
+  #user 'root'
+  #group 'root'
+  user node['hadoop']['user']
+  group node['hadoop']['user']
+  code <<-SHELL
+    tar -xzf #{node['hadoop']['src']}#{node['hadoop']['archive_name']}
+  SHELL
 #  code <<-SHELL
 #    wget -P /usr/local/src/ -ivh http://archive.cloudera.com/cdh5/one-click-install/redhat/6/x86_64/cloudera-cdh-5-0.x86_64.rpm
 #    rpm -ivh /usr/local/src/cloudera-cdh-5-0.x86_64.rpm
 #    rpm --import http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera
 #  SHELL
-#end
+end
 
-# 4. hadoop install
-#%w(hadoop hadoop-conf-pseudo).each do |package_name|
-#  package package_name do
-#    action :install
-#    source "#{node['hadoop']['src']}#{node['hadoop']['package_name']}"
-#    provider Chef::Provider::Package::Rpm
-#  end
-#end
-## 4. hadoop start
-#%w(
-#    hadoop-datanode
-#    hadoop-jobtracker
-#    hadoop-namenode
-#    hadoop-secondarynamenode
-#    hadoop-tasktracker
-#).each do |service_name|
-#    service service_name do
-#        action [ :enable, :start ]
-#        supports :start => true, :stop => true, :restart => true
-#    end
-#end
-# 5. enviroment variable
+# 4. enviroment variable
 template ".bash_profile" do
   source "bash_profile.erb"
   path "/home/#{node['hadoop']['user']}/.bash_profile"
   mode '0644'
   owner node['hadoop']['user']
   group node['hadoop']['group']
-  not_if "grep JAVA_HOME /home/#{node['hadoop']['user']}/.bash_profile"
+  #not_if "grep JAVA_HOME /home/#{node['hadoop']['user']}/.bash_profile"
 end
-# 6. xml attachment
 
-# 7.hadoop auto start
+# 5. xml attachment
+%W(
+  core-site.xml
+  hdfs-site.xml
+  mapred-site.xml
+  yarn-site.xml
+).each do |xml|
+    template "#{xml}" do
+    path "#{node['hadoop']['home']}/etc/hadoop/#{xml}"
+    mode '0644'
+    owner node['hadoop']['user']
+    group node['hadoop']['group']
+  end
+end
+
+# 6. set hadoop env
+bash "set_hadoop_env" do
+  user node['hadoop']['user']
+  code <<-SHELL
+    echo JAVA_HOME=/usr/lib/jvm/jre >> #{node['hadoop']['home']}/etc/hadoop/hadoop-env.sh
+  SHELL
+  not_if "grep JAVA_HOME #{node['hadoop']['home']}/etc/hadoop/hadoop-env.sh"
+end
+
+# 7.hadoop start
+bash "hadoop_start" do
+  cwd "/home/#{node['hadoop']['user']}"
+  user node['hadoop']['user']
+  code <<-SHELL
+    source /home/#{node['hadoop']['user']}/.bash_profile
+    #{node['hadoop']['home']}/bin/hdfs namenode -format
+    #{node['hadoop']['home']}/sbin/start-dfs.sh
+    #{node['hadoop']['home']}/sbin/start-yarn.sh
+  SHELL
+end
